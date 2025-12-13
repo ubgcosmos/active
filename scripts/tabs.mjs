f3752f56bca304a6ce1393daba94eaebfdd025ff
stuff <ethanytang2011@gmail.com>
2025-12-12 20:04:57 -0800

    Change default tab and GitHub link to new URLs

--- scripts/tabs.mjs
+++ scripts/tabs.mjs
@@ -51,7 +51,7 @@ pageRefresh.onclick = () => {
 };
 
 newTabButton.onclick = () => {
-  addTab("uvsearch.rhw.one");
+  addTab("ubgcosmos.github.io/search");
 };
 
 // Options (opt menu)
@@ -75,7 +75,7 @@ abcOption.onclick = () => {
 };
 
 gitOption.onclick = () => {
-  window.open("https://github.com/rhenryw/UV-Static-2.0", "_blank");
+  window.open("https://ubgcosmos.github.io", "_blank");
 };
 
 urlForm.onsubmit = async (e) => {
@@ -133,7 +133,7 @@ const tabItem = (tab) => {
             if (tabs.length) focusTab(tabs[tabs.length - 1]);
             else
               setTimeout(() => {
-                addTab("uvsearch.rhw.one");
+                addTab("ubgcosmos.github.io/search");
               }, 100);
           }
 
@@ -248,7 +248,7 @@ async function addTab(link) {
   focusTab(tab);
 }
 
-addTab("uvsearch.rhw.one");
+addTab("ubgcosmos.github.io/search");
 
 const urlParams = new URLSearchParams(window.location.search);
 
